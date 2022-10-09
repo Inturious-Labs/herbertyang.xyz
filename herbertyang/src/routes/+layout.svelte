0a38f7d51ac29630c5020ff4eff2df42c9d101a3
Herbert <hy.mymail@gmail.com>
2022-10-09 22:09:06 +0800

    Use prism CSS for code blocks

--- herbertyang/src/routes/+layout.svelte
+++ herbertyang/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
     import Header from '$lib/components/Header.svelte'
     import '$lib/styles/style.css'
+    import '$lib/styles/prism-coldark-cold.css'
+ // import '$lib/styles/prism-one-light.css'
 </script>
 
 <Header />
