991d63de7a5b18b318e9274adaa4d40f5e82388e
Herbert <hy.mymail@gmail.com>
2022-10-16 11:52:29 +0800

    Add awesome IC json and svelte page

---
+++ herbertyang/src/routes/awesome-ic/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import teams from "/src/lib/ic/awesome_ic_teams.json"
+</script>
+
+<svelte:head>
+	<title>Awesome IC</title>
+	<meta data-key="description" name="description" content="directory of IC teams">
+</svelte:head>
+
+<h1>Awesome IC</h1>
+
+Directory of project teams in the IC ecosystem
+
+<ol>
+	{#each teams.teams as { 
+        teamSite, 
+        teamName, 
+        teamCity, 
+        teamRegion,
+        teamTwitter
+        }, i}
+		<li>
+            <a href={teamSite}> {teamName}</a>,
+            <a href={teamTwitter}>Twitter</a>,
+            {teamCity}, 
+            {teamRegion}
+		</li>
+	{/each}
+</ol>
