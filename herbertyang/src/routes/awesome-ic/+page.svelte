cc71c65bebd003592c7e18fd83896c7c17e95cdd
Herbert <hy.mymail@gmail.com>
2023-02-17 20:13:41 +0800

    Fetch() works now, but can't export values out

--- herbertyang/src/routes/awesome-ic/+page.svelte
+++ herbertyang/src/routes/awesome-ic/+page.svelte
@@ -1,12 +1,21 @@
 <script>
-    import teams from "/src/lib/ic/awesome_ic_teams.json"
-    import apps from "/src/lib/ic/awesome_ic_apps.json"
+	import { onMount } from "svelte";
+
+    // Use 'import' directly to load json files on the same server
+    // import teams from "/src/lib/ic/awesome_ic_teams.json"
+    // import apps from "/src/lib/ic/awesome_ic_apps.json"
 
     /* 
     // https://scottspence.com/posts/fetch-data-from-two-or-more-endpoints-in-svelte
     // https://www.sitepoint.com/svelte-fetch-data/
     // https://languageimperfect.com/2021/02/17/data-fetching-in-svelte.html
     // https://www.reddit.com/r/sveltejs/comments/z2lxww/how_to_import_a_json_file_from_public_directory/
+    // https://betterprogramming.pub/how-to-fetch-files-from-github-in-javascript-e0ed2c72aeb4
+    // https://www.koderhq.com/tutorial/svelte/http-fetch/
+    // https://sveltesociety.dev/recipes/component-recipes/using-fetch-to-consume-apis
+    // https://www.techiediaries.com/consume-json-rest-api-in-svelte-with-fetch-and-onmount/
+    // https://reactgo.com/svelte-fetch-data-api/
+    // https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
 
     export async function load({ fetch }) {
       const [appsReq, teamsReq] = await Promise.all([
@@ -27,8 +36,45 @@
     export apps, teams
     */
 
-    let unique_type_set = new Set(apps.apps.map(category=>category.appType))
-    const unique_type_json = Array.from(unique_type_set)
+    /*
+    let apps = []
+    let teams = []
+
+    async () => {
+        const appsReq = await fetch('https://raw.githubusercontent.com/zire/awesome-IC/2023/ic/awesome_ic_apps.json');
+        console.log(appsReq)
+        apps = await appsReq.json();
+        const teamsReq = await fetch('https://raw.githubusercontent.com/zire/awesome-IC/2023/ic/awesome_ic_teams.json');
+        teams = await teamsReq.json();
+    };
+
+    console.log(apps);
+    */
+
+    let apps = []
+    fetch('https://raw.githubusercontent.com/zire/awesome-IC/2023/ic/awesome_ic_apps.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let apps = data; 
+            // console.log(apps)
+        })
+
+    console.log("hellow")
+
+    let teams = []
+    fetch('https://raw.githubusercontent.com/zire/awesome-IC/2023/ic/awesome_ic_teams.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let teams = data; 
+            // console.log(teams)
+        })
+
+    let unique_type_set = new Set(apps.apps.map(category=>category.appType));  
+    const unique_type_json = Array.from(unique_type_set);
 
 </script>
 
